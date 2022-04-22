e5228b98b735ee2a25cfb4c6b7bdfb8ef2d59717
husanboy07 <102301397+husanboy07@users.noreply.github.com>
2022-04-22 18:30:01 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// // Get the modal
+// var modal = document.getElementById("myModal");
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+//  alert  ('Saytimizga xush kelibsiz');
+
+
+const img = document.querySelector('.image1');
+const text = document.querySelector('.image1');
+
+function max(){
+    img.style.width = "30%"
+     container.style.display = "flex";
+     container.style.justifyContent = "center"
+     container.style.alignItmes = "center"
+}
+function min(){
+    img.style.width = "250px"
+}
+
+// function max(){
+//     img.style.width = '30%';
+//     text.style.visibility = 'visible';
+//     img.style.borderRadius = '20px';
+// }
+// function min(){
+//     img.style.width = '40%';
+//     text.style.visibility = 'hidden';
+//     img.style.borderRadius = '0px';
+// }
